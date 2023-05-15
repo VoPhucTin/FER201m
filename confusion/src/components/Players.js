2fd5f1750eaadb9e84e93af5e5e785e2add6d2a7
VoPhucTin <kimtintin2@gmail.com>
2023-05-15 21:21:26 +0700

    State Hook

--- confusion/src/components/Players.js
+++ confusion/src/components/Players.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { PlayerList } from '../share/ListOfPlayers';
 
 const PlayersPresentation = () => {
+  const [player, setPlayer] = useState([])
   return (
     <div className='container'>
       {PlayerList.map((player)=>(
@@ -10,11 +11,21 @@ const PlayersPresentation = () => {
             <img src={player.img}/>
             <h3>{player.name}</h3>
             <h4 className='title'>{player.club}</h4>
-            <p><button>Detail</button></p>
+            
+              <a onClick={()=>{setPlayer(player)}} href='#popup1' id='openPopUp'> <span>Detail</span></a>
+               
+
         </div>
         </div>
       ))}
-        
+        <div id='popup1' className='overlay'>
+          <div className='popup'>
+          <img src={player.img}/>
+            <h3>{player.name}</h3>
+            <a className='close' href='#'>&times;</a>
+            <h4 className='content'>{player.info}</h4>
+          </div>
+        </div>
         
     </div>
   )
